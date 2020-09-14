e32ff910addaa81b9daa101441384d0101dcf4ae
piankabianka <bianka.duda98@gmail.com>
2020-09-14 11:05:42 +0200

    C++ script connected

--- drawGrains.js
+++ drawGrains.js
@@ -1,9 +1,11 @@
 //DATA
 const data = require('./drawGrid.js');
+//module.exports = require("bindings")("addon");
+const addon = require('./build/Release/addon.node');
 
 const stepButton=document.querySelector('#step');
 
 
 stepButton.addEventListener('click', function(e){
-    console.log(data.getData());
+    console.log(addon.hello());
 })
--- main.js
+++ main.js
@@ -24,6 +24,7 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
+  app.allowRendererProcessReuse = false;
   createWindow()
   
   app.on('activate', function () {
